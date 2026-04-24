a4e2e8e4a3e101f594e07c9952aea53121f8196a
Shouq <221681782+shoogalzain-cpu@users.noreply.github.com>
2026-04-24 18:25:57 +0300

    + l10n

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get arabic => 'Arabic';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get dark => 'Dark';
+
+  @override
+  String get light => 'Light';
+}
